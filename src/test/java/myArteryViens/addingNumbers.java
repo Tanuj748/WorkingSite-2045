875ec9fe9ab222a6cf7cef2b9039d2c7b02a912e
CharuLata <charu45@gmail.com>
2024-09-05 16:09:43 +0530

    First Commitment

---
+++ src/test/java/myArteryViens/addingNumbers.java
@@ -0,0 +1,15 @@
+package myArteryViens;
+
+public class addingNumbers {
+
+	public static void main(String[] args) 
+	{
+		
+		int i1 = 10 ; 
+		int i2 = 20 ;
+		int sum = i1+i2;
+		
+		System.out.println(sum);
+	}
+
+}
